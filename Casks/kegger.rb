e808a578878c442532470c2349fa00813e66018f
David Beale <david.beale@bealearts.co.uk>
2024-02-19 21:32:08 +0000

    fix no_check

--- Casks/kegger.rb
+++ Casks/kegger.rb
@@ -2,7 +2,7 @@ cask "kegger" do
   arch arm: "arm64", intel: "amd64"
 
   version "2.0.0-alpha1"
-  sha256 ":no-check"
+  sha256 :no_check
 
   url "https://github.com/bealearts/kegger/releases/download/v#{version}/Kegger-#{arch}.dmg"
   name "Kegger"
