eada07024480544f81405da37dafb9d49c763d7c
David Beale <david.beale@bealearts.co.uk>
2024-02-21 18:10:37 +0000

    v2.0.0-alpha5

--- Casks/kegger.rb
+++ Casks/kegger.rb
@@ -1,7 +1,7 @@
 cask "kegger" do
   arch arm: "arm64", intel: "amd64"
 
-  version "2.0.0-alpha2"
+  version "2.0.0-alpha5"
   sha256 :no_check
 
   url "https://github.com/bealearts/kegger/releases/download/v#{version}/Kegger-#{arch}.dmg"
