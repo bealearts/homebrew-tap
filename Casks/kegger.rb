bdd455e01f3da93c903428186a9e40ed2a94a296
David Beale <david.beale@bealearts.co.uk>
2024-02-22 17:59:20 +0000

    style fix

--- Casks/kegger.rb
+++ Casks/kegger.rb
@@ -2,7 +2,7 @@ cask "kegger" do
   arch arm: "arm64", intel: "amd64"
 
   version "2.0.0"
-  sha256 arm: "2678ad167350f51529500550de3d83d5e962511f040b18108ae6c080448ec80f",
+  sha256 arm:   "2678ad167350f51529500550de3d83d5e962511f040b18108ae6c080448ec80f",
          intel: "3f48af6ed077a7f0152245035b89f30ce89c26d887f6314feaef7648ac6316de"
 
   url "https://github.com/bealearts/kegger/releases/download/v#{version}/Kegger-#{arch}.dmg"
