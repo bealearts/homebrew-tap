5ead24d78935a28da6c555a31e122082eacfcbc8
David Beale <DavidBeale@users.noreply.github.com>
2025-01-16 20:54:08 +0000

    v2.1.0

--- Casks/kegger.rb
+++ Casks/kegger.rb
@@ -1,9 +1,9 @@
 cask "kegger" do
   arch arm: "arm64", intel: "amd64"
 
-  version "2.0.3"
-  sha256 arm:   "28762e41cc32219e405e93fc3c4ab9332ef79a2866ba2e69f73ea35c46e63548",
-         intel: "2542d7b3833d8d8583eb633882b171500fc360a7049d72e19d64aef54be1aa46"
+  version "2.1.0"
+  sha256 arm:   "2e9a7869f1ed2e5efad1ce4cc8601d36dd0f826e692e4cc1b65feebe69d3561c",
+         intel: "ebcef2a61eb54cd3682bb9b3d1816e76c4960f6bb88140d5d6eee6b1bef9674d"
 
   url "https://github.com/bealearts/kegger/releases/download/v#{version}/Kegger-#{arch}.dmg"
   name "Kegger"
